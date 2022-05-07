fdd8b84b3d39a2c9b75a3df7f2728a848f7a397c
MSAditya-GL <105115818+MSAditya-GL@users.noreply.github.com>
2022-05-07 19:52:38 +0530

    Add files via upload

---
+++ Fundamentals of Java/src/ForLoop.java
@@ -0,0 +1,11 @@
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		// Aim to print odd numbers b/w 1 to 10
+		for (int i= 1 ; i<=10; i+=2)
+			System.out.println(i);
+		
+	}
+
+}
---
+++ Fundamentals of Java/src/WhileDemo.java
@@ -0,0 +1,11 @@
+
+public class WhileDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		while(true) {
+			System.out.println("inside while");
+		}
+	}
+
+}
